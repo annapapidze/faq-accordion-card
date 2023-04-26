46a96c3d83a01d2d9cea14c36a3f0be9b6cd173b
annapapidze <123008280+annapapidze@users.noreply.github.com>
2023-04-26 12:13:12 +0400

    js card challenge is complete

---
+++ index.js
@@ -0,0 +1,16 @@
+const mainQuestion = document.querySelectorAll(".question");
+
+mainQuestion.forEach((question) => question.addEventListener("click", () => {
+
+    if (question.parentNode.classList.contains("active")) {
+
+        question.parentNode.classList.toggle("active");
+
+    } else {
+        document.querySelectorAll(".question").forEach(question =>
+            question.parentNode.classList.remove("active"));
+
+        question.parentNode.classList.add("active");
+    }
+
+}))
